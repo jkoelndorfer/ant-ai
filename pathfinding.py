7dd6219ba136cbcafded447839164284a088a2b2
John Koelndorfer <jkoelndorfer@gmail.com>
2015-01-25 14:08:06 -0600

    pathfinding.py: Initial A* implementation.
    This looks to be working, but needs further testing.

--- pathfinding.py
+++ pathfinding.py
@@ -1,4 +1,6 @@
+import functools
 import math
+from queue import PriorityQueue
 
 from gameboard import Coordinate as C
 
@@ -7,18 +9,57 @@ class Pathfinder(object):
     def __init__(self, gameboard):
         self.gameboard = gameboard
 
-    def find_path(start, end):
+    # Used http://web.mit.edu/eranki/www/tutorials/search as a reference for
+    # this A* implementation.
+    def find_path(self, start, end):
         """
         Given Coordinates start and end, finds the shortest path between them
         on the gameboard.
         """
-        raise NotImplementedError()
+        open_coords = PriorityQueue()
+        parent_coords = dict()
+        g_score = dict()
+        f_score = dict()
+
+        open_coords.put(QueueCoordinate(coordinate=start, f_score=0))
+        f_score[start] = self.heuristic_cost(start, end)
+        g_score[start] = 0
+
+        while open_coords.qsize() > 0:
+            q = open_coords.get().coordinate
+            successors = self._get_neighboring_coordinates(q)
+            for successor in successors:
+                if successor == end:
+                    parent_coords[successor] = q
+                    return self.build_path(end, parent_coords)
+                # All traversals have equal cost
+                successor_g = g_score[q] + 1
+                successor_h = self.heuristic_distance(successor, end)
+                successor_f = successor_g + successor_h
+                if f_score.get(successor, successor_f + 1) > successor_f:
+                    f_score[successor] = successor_f
+                    g_score[successor] = successor_g
+                    open_coords.put(QueueCoordinate(successor, successor_f))
+                    parent_coords[successor] = q
+
+    def build_path(self, end, parent_coords):
+        current = end
+        path = list()
+        path.append(end)
+        while True:
+            parent = parent_coords.get(current, None)
+            if parent is None:
+                break
+            path.insert(0, current)
+            current = parent
+        return path
 
     @classmethod
-    def distance(cls, start, end):
-        return math.sqrt(
-            (start.x - end.x)**2 + (start.y - end.y)**2
-        )
+    def heuristic_distance(cls, start, end):
+        # We don't need to take the square root to find the actual distance.
+        # As long as we compare the squares of the distances, our comparisons
+        # will end up the same.
+        return ((start.x - end.x)**2 + (start.y - end.y)**2)
 
     def heuristic_cost(self, start, end):
         """
@@ -38,14 +79,27 @@ class Pathfinder(object):
             else:
                 new_end_y = end.y - self.gameboard.height
         end = C(new_end_x, new_end_y)
-        return self.distance(start, end)
+        return self.heuristic_distance(start, end)
 
-    def _get_neighboring_tiles(self, tile):
+    def _get_neighboring_coordinates(self, coordinate):
         gt = self.gameboard.get_tile
-        coordinate = tile.coordinate
-        return [
+        neighbors = (
             gt(C(coordinate.x + 1, coordinate.y)),
             gt(C(coordinate.x - 1, coordinate.y)),
             gt(C(coordinate.x, coordinate.y + 1)),
             gt(C(coordinate.x, coordinate.y - 1))
-        ]
+        )
+        return tuple((x.coordinate for x in neighbors if x.traversable))
+
+
+@functools.total_ordering
+class QueueCoordinate(object):
+    def __init__(self, coordinate, f_score):
+        self.coordinate = coordinate
+        self.f_score = f_score
+
+    def __lt__(self, other):
+        return (self.f_score < other.f_score)
+
+    def __eq__(self, other):
+        return (self.f_score == other.f_score)
